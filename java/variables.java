571bb335f73c22e333b9028ec8d6b33727600b05
Tech-gamer271 <agastya.naik27@gmail.com>
2025-03-25 06:27:05 +0000

    commit_variables

---
+++ java/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        int mum = 7;
+        int mum2 = 8;
+        System.out.println(mum + mum2);
+        System.out.println(mum - mum2);
+        System.out.println(mum * mum2);
+        System.out.println(mum / mum2);
+        System.out.println(mum % mum2);
+    }
+}
